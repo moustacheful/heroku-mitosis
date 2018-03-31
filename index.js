1a18fc3ed36c806baaac3b54ad7f01ddf3fe1a5c
Daniel Acuña <suchious@gmail.com>
2018-03-31 14:21:54 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const Heroku = require('heroku-client');
+const Promise = require('bluebird');
+const _ = require('lodash');
+const HerokuApp = require('./lib/heroku-app');
+const args = require('yargs').argv;
+
+const action = args._[0];
+const config = require('./app-config.json');
+const options = config.__mitosis;
+delete options.__mitosis;
+
+process.exit(1);
+
+const missingArgs = _.difference(['apiKey', 'name'], Object.keys(args));
+if (missingArgs.length) {
+	console.error('Missing required arguments: ' + missingArgs.join(', '));
+	process.exit(1);
+}
+
+const h = new Heroku({ token: args.apiKey });
+
+async function createReviewApp() {
+	console.log('Looking for seed app...');
+	const seedApp = await h.get(`/apps/${options.seed}`);
+	const configVars = await h.get(`/apps/${options.seed}/config-vars`);
+
+	const newAppName = args.name;
+
+	const forkedExists = await h
+		.get(`/apps/${newAppName}`)
+		.then(() => {
+			console.log('Application', newAppName, 'already exists, bailing!');
+			return true;
+		})
+		.catch(err => false);
+
+	if (forkedExists) {
+		process.exit(0);
+	}
+
+	console.log(
+		'Creating new app:',
+		newAppName,
+		'in region:',
+		seedApp.region.name
+	);
+
+	const forkedApp = await h.post('/apps', {
+		body: {
+			name: newAppName,
+			region: seedApp.region.id,
+		},
+	});
+
+	const forked = new HerokuApp(forkedApp, h);
+	await forked.updateConfigVars(configVars);
+
+	if (config.addons) await forked.createAddons(config.addons);
+	if (config.buildpacks) await forked.setBuildpacks(config.buildpacks);
+	if (options.config_vars)
+		await forked.setDynamicConfigVars(options.config_vars);
+}
+
+async function destroyReviewApp() {
+	await h.delete(`/apps/${args.name}`);
+}
+
+async function scale() {
+	const forked = new HerokuApp({ name: args.name }, h);
+	if (config.formation) await forked.setFormation(config.formation);
+}
+
+let fn;
+switch (action) {
+	case 'create':
+		fn = createReviewApp;
+		break;
+
+	case 'destroy':
+		fn = destroyReviewApp;
+		break;
+
+	case 'scale':
+		fn = scale;
+		break;
+
+	default:
+		console.error('Action not recognized, bailing.');
+		process.exit(0);
+		break;
+}
+
+fn().catch(err => {
+	console.log(err);
+	process.exit(1);
+});
